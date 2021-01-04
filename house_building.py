f81b407e8038de0cef773506f2ffa632ca9879e5
aostapjuk <aostapjuk@gmail.com>
2021-01-04 18:29:51 +0200

    Описано рисование домика, но функции для фундамента, стен, крыши, окна - заглушки. Нужно их реализовать по существу.

--- house_building.py
+++ house_building.py
@@ -6,11 +6,35 @@ win_height = 500
 def main():
     draw_house(win_width//3, win_height*2//3, 150, 200)
 
-def draw_house(x0, y0, ):
+def draw_house(x0, y0, width, height):
     ''' Функция рисует дом в положении  на холсте.
         x0, y0 - центральная нижняя точка домика
         width, height - ширина и высота.
     '''
+    foundation_height = int(0.1*height)
+    walls_height = int(0.5*height)
+    walls_width = int(0.9*width)
+    roof_height = height - walls_height - foundation_height
+    window_height = width//3
+    window_width = walls_height//3
+    
+    draw_foundation(x0, y0, width, foundation_height)
+    draw_walls(x0, y0-foundation_height,
+               walls_width, walls_height)
+    draw_window(x0, y0-foundation_height-walls_height//3,
+                window_height, window_width)
+    draw_roof(x0, y0-walls_height, width, roof_height)
+
+def draw_foundation(x0, y0, width, height):
+    pass
+
+def draw_walls(x0, y0, width, height):
+    pass
+
+def draw_window(x0, y0, width, height):
+    pass
+
+def draw_roof(x0, y0, width, height):
     pass
 
 main()
