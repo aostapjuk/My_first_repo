bba4fae05a9cb03b72154041c58729aaa54a5827
aostapjuk <aostapjuk@gmail.com>
2021-01-07 20:59:55 +0200

    Сделал анимацию домика. Реализовал возврат отрисованных элементов из каждой функции Все элементы домика смещаються при клике мыши.

--- house_building.py
+++ house_building.py
@@ -4,9 +4,15 @@ win_width = 600
 win_height = 500
 
 def main():
-    global win
+    global win, house_elements
     win = GraphWin('Picture graphics', win_width, win_height)
-    draw_house(win_width//3, win_height*2//3, 150, 200)
+    house_elements = draw_house(win_width//3, win_height*2//3, 150, 200)
+    cursor_point = win.getMouse()
+    for elem in house_elements:
+        elem.move(50, 0)
+    cursor_point = win.getMouse()
+    for elem in house_elements:
+        elem.move(0, 50)
     cursor_point = win.getMouse()
     win.close()
 
@@ -14,6 +20,7 @@ def draw_house(x0, y0, width, height):
     ''' Функция рисует дом в положении  на холсте.
         x0, y0 - центральная нижняя точка домика
         width, height - ширина и высота.
+        return: список нарисованных объектов
     '''
     foundation_height = int(0.1*height)
     walls_height = int(0.5*height)
@@ -22,12 +29,13 @@ def draw_house(x0, y0, width, height):
     window_height = width//3
     window_width = walls_height//3
     
-    draw_foundation(x0, y0, width, foundation_height)
-    draw_walls(x0, y0-foundation_height,
-               walls_width, walls_height)
-    draw_window(x0, y0-foundation_height-walls_height//3,
-                window_height, window_width)
-    draw_roof(x0, y0-foundation_height-walls_height, width, roof_height)
+    foundation = draw_foundation(x0, y0, width, foundation_height)
+    walls = draw_walls(x0, y0-foundation_height,
+                       walls_width, walls_height)
+    house_window = draw_window(x0, y0-foundation_height-walls_height//3,
+                               window_height, window_width)
+    roof = draw_roof(x0, y0-foundation_height-walls_height, width, roof_height)
+    return  foundation + walls + roof + house_window
 
 def draw_foundation(x0, y0, width, height):
     foundation = Rectangle(Point(x0-width//2, y0-height), Point(x0+width//2, y0))
@@ -35,6 +43,7 @@ def draw_foundation(x0, y0, width, height):
     foundation.setFill('brown')
     foundation.draw(win)
     print('foundation', x0, y0, width, height)
+    return [foundation]
 
 def draw_walls(x0, y0, width, height):
     walls = Rectangle(Point(x0-width//2, y0-height), Point(x0+width//2, y0))
@@ -42,6 +51,7 @@ def draw_walls(x0, y0, width, height):
     walls.setFill('green')
     walls.draw(win)
     print('walls', x0, y0, width, height)
+    return [walls]
 
 def draw_window(x0, y0, width, height):
     window = Rectangle(Point(x0-width//2, y0-height), Point(x0+width//2, y0))
@@ -49,6 +59,7 @@ def draw_window(x0, y0, width, height):
     window.setFill('yellow')
     window.draw(win)
     print('walls', x0, y0, width, height)
+    return [window]
 
 def draw_roof(x0, y0, width, height):
     coordinates = [(x0-width//2, y0), (x0, y0-height),
@@ -59,5 +70,6 @@ def draw_roof(x0, y0, width, height):
     roof.setFill('darkred')
     roof.draw(win)
     print('roof', x0, y0, width, height)
+    return [roof]
 
 main()
