35851f6bdc67b6a652b8f644db3782dfcf4d7be7
aostapjuk <aostapjuk@gmail.com>
2021-01-09 13:23:51 +0200

    Сделал овал бегающим.
    Реагирую на клик мышки. Овал переносится по клику к курсору мышки.Понажатию на Stop овал исчезает.

--- ball_game/canvas_example.py
+++ ball_game/canvas_example.py
@@ -1,16 +1,38 @@
 from tkinter import *
 
+oval_id = None
 
 def create_ball():
-    c.create_oval(10, 10, 100, 100, fill='red')
+    global oval_id
+    if oval_id is None:
+        oval_id = c.create_oval(10, 10, 100, 100, fill='red')
+    else:
+        print('ПРЕДУПРЕЖДЕНИЕ! Овал уже создан. Не надо нажимать эту кнопку!')
+
+
+def delete_ball():
+    global oval_id
+    c.delete(oval_id)
+    oval_id = None
+
+
+def click_handler(event):
+    print(event.x, event.y)
+    if oval_id:
+        c.coords(oval_id, event.x-5, event.y-5, event.x+5, event.y+5)
 
 
 root = Tk('Главное окно')
 root.geometry('640x480')
+buttons_panel = Frame()
+buttons_panel.pack(side=TOP, anchor='nw')
+button_start = Button(buttons_panel, text='Start', command=create_ball)
+button_start.pack(side=LEFT)
+button_stop = Button(buttons_panel, text='Stop', command=delete_ball)
+button_stop.pack(side=LEFT)
 
 c = Canvas(root, width=640, height=400, bg='white')
-c.pack()
-button_start = Button(root, text='Start', command=create_ball)
-button_start.pack()
+c.pack(side=TOP, fill=X, anchor='nw')
+c.bind('<Button>', click_handler)
 
 root.mainloop()
