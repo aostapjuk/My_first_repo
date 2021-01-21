8b7c23c1d0db61f4e74252cf01dbc7cca8256ceb
aostapjuk <aostapjuk@gmail.com>
2021-01-21 10:26:42 +0200

    Рефакторинг состоялся.
    - Вынесли все функции, связанные с шариком и его движением в class Ball.
    - Расхаркодили константы 640х480, создав глобально canvas_width, canwas_height.

--- graviti_ball/graviti_ball.py
+++ graviti_ball/graviti_ball.py
@@ -2,12 +2,14 @@ from tkinter import *
 from random import randint
 import time
 
+canvas_width = 640
+canvas_height = 480
 # Режим игры - игра идёт или нет.
 game_began = False
 initial_balls_number = 5
 scores = 0
 sleep_time = 50     # ms
-# Список шариков, каждый из которых - список: [x, y, dx, dy, r, oval_id]
+# Список объектов типа Ball
 balls = []
 
 
@@ -36,61 +38,57 @@ def button_stop_game_handler():
 # -----------------GAME MODEL:-------------------
 def game_start():
     for i in range(initial_balls_number):
-        ball = ball_create()
+        ball = Ball()
         balls.append(ball)
 
 
 def game_stop():
     for ball in balls:
-        ball_delete(ball)
+        ball.delete()
 
 
 def game_step():
     for ball in balls:
-        ball_step(ball)
+        ball.step()
 
 
-def ball_create():
-    global scores
-    scores = 0
-    scores_text["text"] = "Ваши очки: 0"
-    r = randint(10, 30)
-    x = randint(0 + r, 639 - r)
-    y = randint(0 + r, 479 - r)
-    dx = randint(-4, 4)
-    dy = randint(-4, 4)
-    oval_id = c.create_oval(x-r, y-r, x+r, y+r, fill='red')
-    ball = [x, y, dx, dy, r, oval_id]
-    return ball
+class Ball:
+    def __init__(self):
+        '''global scores
+        scores = 0
+        scores_text["text"] = "Ваши очки: 0"'''
+        self.r = randint(10, 30)
+        self.x = randint(0 + self.r, canvas_width - self.r)
+        self.y = randint(0 + self.r, canvas_height - self.r)
+        self.dx = randint(-4, 4)
+        self.dy = randint(-4, 4)
+        self.oval_id = c.create_oval(self.x-self.r, self.y-self.r,
+                                     self.x+self.r, self.y+self.r,
+                                     fill='red')
 
+    def step(self):
+        """
+        Сдвигает шарик ball в соответствии с его скоростью.
+        """
+        if self.oval_id is not None:
+            self.x += self.dx
+            self.y += self.dy
+            if self.x+self.r >= canvas_width or self.x-self.r <= 0:
+                self.dx = -self.dx
+            if self.y+self.r >= canvas_height or self.y-self.r <= 0:
+                self.dy = -self.dy
+            c.coords(self.oval_id,
+                     self.x - self.r, self.y - self.r,
+                     self.x + self.r, self.y + self.r)
 
-def ball_step(ball):
-    """
-    Сдвигает шарик ball в соответствии с его скоростью.
-    :param ball: это список (x, y, dx, dy, r, oval_id)
-    """
-    x, y, dx, dy, r, oval_id = ball
-    if oval_id is not None:
-        x += dx
-        y += dy
-        if x+r >= 639 or x-r <= 0:
-            dx = -dx
-        if y+r >= 439 or y-r <= 0:
-            dy = -dy
-        c.coords(oval_id, x - r, y - r, x + r, y + r)
-    ball[:] = x, y, dx, dy, r, oval_id
-
-
-def ball_delete(ball):
-    x, y, dx, dy, r, oval_id = ball
-    c.delete(oval_id)
-    oval_id = None
-    ball[:] = x, y, dx, dy, r, oval_id
+    def delete(self):
+        c.delete(self.oval_id)
+        self.oval_id = None
 
 
 # -----------------GAME VIEW:-------------------
 root = Tk('Игра "Поймай шарики"')
-root.geometry('640x480')
+#root.geometry('640x480')
 
 buttons_panel = Frame(root, bg='gray')
 buttons_panel.pack(fill=BOTH)
@@ -105,8 +103,8 @@ time_label.pack(side=LEFT)
 scores_text = Label(buttons_panel, text='Ваши очки: 0')
 scores_text.pack(side=RIGHT)
 
-c = Canvas(root, bg='white')
-c.pack(fill=BOTH, expand=1)
+c = Canvas(root, bg='white', width=canvas_width, height=canvas_height)
+c.pack(anchor='nw', fill=BOTH, expand=1)
 
 time_label.after_idle(tick)
 root.mainloop()
